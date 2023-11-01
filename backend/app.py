298293d00eddb6ad13f3e3e9efe9ea6ad803bfc6
우성현 <wshdhkd@naver.com>
2023-11-01 17:44:38 +0900

    server basic setting

---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO
+import cv2
+
+app = Flask(__name__)
+
+
+# YOLO 모델 초기화 및 설정
+yolo_cfg = 'path_to_yolo_cfg_file.cfg'
+
+yolo_weights = 'path_to_yolo_weights.weights'
+
+net = cv2.dnn.readNet(yolo_cfg, yolo_weights)
+
+# 웹 소켓 연결 설정 (socket.io를 사용할 경우)
+
+
+
+# YOLOv4 모델을 사용하여 라벨을 인식하고 결과를 반환
+def detect_labels(image_data):
+    # 이미지 데이터를 OpenCV 형식으로 변환
+    image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
+
+    return labels
+
+
+# 클라이언트로부터 받아온 이미지 데이터를 처리하는 코드
+@app.route('/process_webcam_data', methods=['POST'])
+def process_webcam_data():
+    data = request.get_json()
+    image_data = data['image_data']
+    result = detect_labels(image_data)  # 이 함수에서 YOLO 모델을 사용하여 라벨을 인식
+
+    # 예측 결과를 클라이언트로 전송
+    return jsonify({'result': result})
+
+socketio = SocketIO(app)
+
+@socketio.on('connect')
+def handle_connect():
+    print('웹 소켓 연결이 수립되었습니다.')
+
+# 클라이언트로부터 데이터를 받아처리하는 소켓 이벤트 정의
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const port = 3000;
+const axios = require('axios');
+
+// Flask AI 서버의 엔드포인트 주소
+const flaskServerUrl = 'http://localhost:5000/process_webcam_data';
+
+// 웹캠 데이터를 Flask 서버로 전송
+function sendWebcamDataToFlask(dataUrl) {
+  axios.post(flaskServerUrl, { image_data: dataUrl })
+    .then(response => {
+      const result = response.data.result;
+      // 처리 결과를 사용하여 필요한 작업을 수행
+    })
+    .catch(error => {
+      console.error('Flask 서버로 데이터 전송 중 오류 발생:', error);
+    });
+}
+
+// 클라이언트에서 웹캠 데이터를 받아오고 Flask 서버로 전송
+// 이미지 데이터를 받아옴
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('웹 소켓 연결이 수립되었습니다.');
+
+  socket.on('webcamData', (dataUrl) => {
+    io.emit('webcamData', dataUrl); // 모든 클라이언트에게 데이터 전송
+  });
+
+  socket.on('disconnect', () => {
+    console.log('웹 소켓 연결이 끊어졌습니다.');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+});
