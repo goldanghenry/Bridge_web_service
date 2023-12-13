c29b12cd48cb39d8fb2f2bef673ace35c2287195
goldanghenry <wshdhkd@naver.com>
2023-12-14 04:34:53 +0900

    yolo+stt api 수정

--- backend/flask_server.py
+++ backend/flask_server.py
@@ -4,6 +4,16 @@ from dotenv import load_dotenv
 # from pydub import AudioSegment
 from openai import OpenAI
 import os
+import numpy as np
+import cv2
+import base64
+import darknet
+from time import time
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+	app.run(debug=True, port=5000)
 
 app = Flask(__name__)
 CORS(app)
@@ -21,6 +31,17 @@ client.api_key = OPENAI_API_KEY
 upload_folder = 'uploads'
 if not os.path.exists(upload_folder):
     os.makedirs(upload_folder)
+    
+# yolo - 성현
+def load_darknet_model(config_file, data_file, weights_file):
+    network, class_names, class_colors = darknet.load_network(config_file, data_file, weights_file, batch_size=1)
+    return network, class_names, class_colors
+    
+# 다크넷 컴파일
+configPath="./darknet/cfg/yolov4-tiny-obj.cfg"
+weightPath="./darknet/backup/yolov4-tiny-obj_best.weights"
+metaPath="./darknet/data/obj.data"
+network, class_names, class_colors = load_darknet_model(configPath, metaPath, weightPath)
 
 @app.route('/upload-audio', methods=['POST'])
 def upload_audio():
@@ -49,6 +70,33 @@ def upload_audio():
 
     return transcript
 
+def inference_on_video(video_path, network, class_names, threshold):
+    cap = cv2.VideoCapture(video_path)
+    detected_classes_list = []
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+        darknet_width,darknet_height = 416, 416
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (darknet_width, darknet_height), interpolation=cv2.INTER_LINEAR)
+
+        img_for_detect = darknet.make_image(darknet_width, darknet_height, 3)
+        darknet.copy_image_from_bytes(img_for_detect, frame_resized.tobytes())
+
+        detections = darknet.detect_image(network, class_names, img_for_detect, thresh=threshold)
+
+        detected_classes = get_detected_classes(detections)
+        print("Detected Classes: {}".format(detected_classes))
+        detected_classes_list.extend(detected_classes)
+
+        darknet.free_image(img_for_detect)
+
+    cap.release()
+    return detected_classes_list
+
 @app.route('/upload-video', methods=['POST'])
 def upload_video():
     if 'video' not in request.files:
@@ -62,11 +110,42 @@ def upload_video():
     video_file_path = os.path.join(upload_folder, 'test_video.mp4')
     video_file.save(video_file_path)
 
-    return jsonify({'success': True, 'message': 'Video file uploaded successfully'}), 200
+    
+    # threshold : 임계값 0~1
+    try:
+        detected_classes_list = inference_on_video(video_file_path, network, class_names, 0.7)
+        result = request_chat_gpt(detected_classes_list)
+        print(result)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500  # 에러 핸들링 추가
 
-@app.route('/llm')
-def home():
-    words = ["저희", "농아인", "일반인", "화상", "회의", "서비스", "브릿지", "만들다", "12", "팀"]
+    return result #jsonify({'detected_classes': detected_classes_list})
+
+
+# @app.route('/upload-video', methods=['POST'])
+# def upload_video():
+#     if 'video' not in request.files:
+#         return jsonify({'error': 'No video file provided'}), 400
+
+#     video_file = request.files['video']
+#     if video_file.filename == '':
+#         return jsonify({'error': 'No selected file'}), 400
+
+#     # 동적으로 파일 경로 설정 (원하는 파일 이름으로 변경)
+#     video_file_path = os.path.join(upload_folder, 'test_video.mp4')
+#     video_file.save(video_file_path)
+    
+#     try:
+#         # 욜로
+#         text = inference(video_file_path)
+#     except Exception as e:
+#         return jsonify({'error': str(e)}), 500  # 에러 핸들링 추가
+    
+#     return text
+
+# @app.route('/llm')
+def request_chat_gpt(detected_classes):
+    words = detected_classes #["저희", "농아인", "일반인", "화상", "회의", "서비스", "브릿지", "만들다", "12", "팀"]
     prompt = f"이 단어들을 자연스러운 한문장으로 만들어줘: {', '.join(words)}"
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
@@ -79,7 +158,65 @@ def home():
 
     generated_text = response.choices[0].message.content
     generated_text = generated_text.split(':')[1][2:-3] # 생성된 문장만 추출
-    return render_template('result.html', generated_text=generated_text)
+    return generated_text
+
+# Yolo
+
+# @app.route('/process_image', methods=['POST'])
+# def process_image():
+#     data = request.get_json()  # JSON 데이터를 파싱
+#     image_data = base64.b64decode(data['image'])
+#     image = np.frombuffer(image_data, dtype=np.uint8)
+#     image = cv2.imdecode(image, flags=cv2.IMREAD_COLOR)
+    
+#     # YOLO 모델을 사용하여 이미지 처리
+#     results = performDetect(image=image)
+
+#     # 감지된 객체의 정보를 반환
+#     return jsonify(results)
+
+# def convertBack(x, y, w, h):
+#     # Convert from center coordinates to bounding box coordinates
+#     xmin = int(round(x - (w / 2)))
+#     xmax = int(round(x + (w / 2)))
+#     ymin = int(round(y - (h / 2)))
+#     ymax = int(round(y + (h / 2)))
+#     return xmin, ymin, xmax, ymax
+
+# def performDetect(image, thresh=0.25, configPath="../darknet/cfg/yolov4-tiny-obj.cfg", weightPath="../darknet/backup/yolov4-tiny-obj_best.weights", metaPath="../darknet/data/obj.data"):
+#     # Load the network
+#     network, class_names, class_colors = darknet.load_network(
+#         configPath,
+#         metaPath,
+#         weightPath,
+#         batch_size=1
+#     )
+    
+#     # Resize the image and convert to RGB
+#     width, height = darknet.network_width(network), darknet.network_height(network)
+#     darknet_image = darknet.make_image(width, height, 3)
+#     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#     image_resized = cv2.resize(image_rgb, (width, height), interpolation=cv2.INTER_LINEAR)
+
+#     # Copy the image data into the darknet image
+#     darknet.copy_image_from_bytes(darknet_image, image_resized.tobytes())
+
+#     # Perform the detection
+#     detections = darknet.detect_image(network, class_names, darknet_image, thresh=thresh)
+#     darknet.free_image(darknet_image)
+
+#     # Convert detections to dictionary
+#     detections_dict = []
+#     for label, confidence, bbox in detections:
+#         xmin, ymin, xmax, ymax = convertBack(float(bbox[0]), float(bbox[1]), float(bbox[2]), float(bbox[3]))
+#         detections_dict.append({
+#             "label": label,
+#             "confidence": float(confidence),
+#             "bbox": [xmin, ymin, xmax, ymax]
+#         })
+
+#     return detections_dict
+
 
 if __name__ == '__main__':
     app.run(debug=True)
